952fc8db2504bc2ff9d0fa18423fed5f2a2bc9b7
celz <celz@localhost.localdomain>
2020-08-11 11:01:09 -0400

    Add 03_Read_Length step

---
+++ Scripts/03_Read_Length/Run_Jobs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+##################################################################################
+#Andy Rampersaud, 02.16.16
+#This script would be used to run Read_Length.pbs in parallel for different FASTQ files
+#Way to run script:
+#Usage: ./Run_Jobs.sh
+#Example: 
+#./Run_Jobs.sh
+##################################################################################
+
+#Clean directory
+rm -rf *.o* *.e*
+
+# export all variables from Pipeline_Setup.conf
+eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
+
+# dir_name and job_name are required in the next steps
+dir_name=$(basename $(pwd))
+step_num=$(echo ${dir_name} | cut -d'_' -f 1)
+job_name="Step_${step_num}"
+
+output_dir="$(pwd)/Job_Summary"
+rm -rf "${output_dir}" && mkdir -p "${output_dir}"
+
+# samples contains array of (sample_dir, sample_id, description) for each sample
+samples=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --samples))
+
+# rename directories from sample_dir to sample_id
+for ((i=0;i< ${#samples[@]} ;i+=3));
+do
+    # sample_dir=${samples[i]}
+    sample_id=${samples[i+1]}
+    # description=${samples[i+2]}
+
+    qsub -N "${job_name}_${sample_id}" -P "${PROJECT}" -l h_rt="${TIME_LIMIT}" Read_Length.qsub "${sample_id}" "${output_dir}"
+done
+
+echo "End of 03_READ_LENGTH commands"
+echo "-----------------------"
+##################################################################################
---
+++ Scripts/03_Read_Length/Summarize_Jobs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+##################################################################################
+#Andy Rampersaud, 02.23.16
+#This script would be used to summarize Read_Length jobs
+#Way to run script:
+#Usage: 
+#./Read_Length_Summary.sh
+##################################################################################
+
+# export all variables from Pipeline_Setup.conf
+eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
+
+# retrieve the dir name for this step:
+dir_name=$(basename $(pwd))
+
+# output dir to store text files:
+output_dir="${DATASET_DIR}/Scripts/${dir_name}/Job_Summary"
+output_file="${output_dir}/Read_Length_Stats.txt"
+
+rm -rf ${output_file} && touch ${output_file}
+
+cd "${output_dir}"
+
+# print headers to the output file
+printf "SAMPLE_ID\tFASTQ_File_Name\tRead_Length(bp)\tRead_Count" >> "${output_file}"
+
+# samples contains array of (sample_dir, sample_id, description) for each sample
+samples=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --samples))
+
+# rename directories from sample_dir to sample_id
+for ((i=0;i< ${#samples[@]} ;i+=3));
+do
+    # sample_dir=${samples[i]}
+    sample_id=${samples[i+1]}
+    # description=${samples[i+2]}
+    echo ${sample_id}
+    cat "${sample_id}_read_length.txt" >> $output_file
+    
+done
+
+echo "Check out ${OUTPUT_FILE}"
+echo "#-----------------------------------------"
+
