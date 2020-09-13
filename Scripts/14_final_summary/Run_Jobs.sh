44639350bfa8180c967013242b9fe7a093fd0e9e
mpyatkov <test@test.com>
2020-09-13 15:58:14 -0400

    fix: fix copying of SEGEX files and add refactoring of updown_genes.R

--- Scripts/14_final_summary/Run_Jobs.sh
+++ Scripts/14_final_summary/Run_Jobs.sh
@@ -6,7 +6,6 @@
 #Example:
 #./Run_Jobs.sh
 # The result fo this script copies all the Diffexp_v2_genebody (.txt) files from DE analysis (HTSeq Method) and copies in the current directory. These files are then used as input bu the Pearson_Script.R to generate pearson correlation plots and matrices. 
-# The . of this script is two folders: 1) Pearson_All (All genes were involved to calculate the pearson) 2) Pearson_Filtered (Genes filtered by rpkm  > 1 are used for calculating the pearson values)
 
 set -o errexit
 set -o pipefail
@@ -60,55 +59,54 @@ cp -rf ${Level_UP}/13_Correlation/Job_Summary/* ./output/
 mv *.pdf ${Level_UP}/14_final_summary/output
 
 # remove from 09abc/Output* all segex files
-set +eu
-find ../09* -name "*_forSEGEXUpload*.txt" | grep Output | xargs -n1 rm -rf 
-set -eu
-
+# set +eu
+# find ../09* -name "*_forSEGEXUpload*.txt" | grep Output | xargs -n1 rm -rf 
+# set -eu
 
 # copy all segex files in 
 # 09a 
 mkdir -p output/Segex_09a/
-find ../09a_DE_* -name "*SEGEX*" | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09a/
+find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09a/
 
 mkdir -p output/Segex_09a/Segex09a_GeneBody=ExonCollapsed
-find ../09a_DE_* -name "*SEGEX*" | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_GeneBody=ExonCollapsed/
+find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_GeneBody=ExonCollapsed/
 
 mkdir -p output/Segex_09a/Segex09a_Intronic_Only
-find ../09a_DE_* -name "*SEGEX*" | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Intronic_Only/
+find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Intronic_Only/
 
 mkdir -p output/Segex_09a/Segex09a_Exonic_Only
-find ../09a_DE_* -name "*SEGEX*" | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Exonic_Only/
+find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Exonic_Only/
 
 # 09b
 
 mkdir output/Segex_09b
-find ../09b_DE_* -name "*SEGEX*" | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/
+find ../09b_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/
 
 mkdir -p output/Segex_09b/Segex09b_GeneBody=ExonCollapsed
-find ../09b_DE_* -name "*SEGEX*" | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_GeneBody=ExonCollapsed/
+find ../09b_DE_* -name "*SEGEX*" | grep -i genebody | grep -iv output | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_GeneBody=ExonCollapsed/
 
 mkdir -p output/Segex_09b/Segex09b_Intronic_Only
-find ../09b_DE_* -name "*SEGEX*" | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Intronic_Only/
+find ../09b_DE_* -name "*SEGEX*" | grep -i intronic | grep -iv output | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Intronic_Only/
 
 mkdir -p output/Segex_09b/Segex09b_Exonic_Only
-find ../09b_DE_* -name "*SEGEX*" | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Exonic_Only/
+find ../09b_DE_* -name "*SEGEX*" | grep -i exonic | grep -iv output | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Exonic_Only/
 
 # 09c
 
 mkdir output/Segex_09c
-find ../09c_DE_* -name "*SEGEX*" | grep -i fpkm | grep -i exoncoll | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/
+find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i exoncoll | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/
 
 mkdir -p output/Segex_09c/Segex09c_ExonCollapsed
-find ../09c_DE_* -name "*SEGEX*" | grep -i exoncoll | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_ExonCollapsed/
+find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i exoncoll | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_ExonCollapsed/
 
 mkdir -p output/Segex_09c/Segex09c_GeneBody
-find ../09c_DE_* -name "*SEGEX*" | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_GeneBody/
+find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_GeneBody/
 
 mkdir -p output/Segex_09c/Segex09c_Intronic_Only
-find ../09c_DE_* -name "*SEGEX*" | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Intronic_Only/
+find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Intronic_Only/
 
 mkdir -p output/Segex_09c/Segex09c_Exonic_Only
-find ../09c_DE_* -name "*SEGEX*" | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Exonic_Only/
+find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Exonic_Only/
 
 # summarize up and down genes
 # 09a
@@ -129,21 +127,8 @@ mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
 
 OUTPUT_FILE="$(pwd)/09c_DE_Genes_counts.csv"
 find ../09c_DE_* -iname "*DE_Gene_C*" | xargs -n1 -I{} cat {} | grep -v "Output_File" >> ${INPUT_UPDOWN_FILE}
-Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE}
+Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE} ${DATASET_LABEL}
 mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
-
-# mv *.csv ${Level_UP}/14_final_summary/output
-#echo 'Setup_Pipeline_DIR :' $Setup_Pipeline_DIR
-# cc=$(find ../ -name "[0-9][0-9]*b_DE_*_FEATURECOUNTS" | wc -l);
-# echo $cc
-# start=1
-# for((i=$start; i <=$cc; i++))
-# { 	
-#     cp -rf ${Level_UP}/09b_DE_${i}_FEATURECOUNTS/Summary_Differential_Expression/SEGEX_Upload_Files/Individual/*_GeneBody_forSEGEXUpload_TPM_EdgeR_featureCounts.txt .
-#     cp -rf ${Level_UP}/09b_DE_${i}_FEATURECOUNTS/Summary_Differential_Expression/SEGEX_Upload_Files/Individual/*_GeneBody_forSEGEXUpload_EdgeR_featureCounts.txt .
-#     cp -rf ${Level_UP}/09c_DE_${i}_LNCRNA/Summary_Differential_Expression/SEGEX_Upload_Files/Individual/*_GeneBody_forSEGEXUpload_TPM_EdgeR_featureCounts.txt .
-#     cp -rf ${Level_UP}/09c_DE_${i}_LNCRNA/Summary_Differential_Expression/SEGEX_Upload_Files/Individual/*_GeneBody_forSEGEXUpload_EdgeR_featureCounts.txt .
-# }
 
 mv *.txt ${Level_UP}/14_final_summary/output	
 echo "All files copied. Done "
--- Scripts/14_final_summary/Scripts/updown_genes.R
+++ Scripts/14_final_summary/Scripts/updown_genes.R
@@ -10,35 +10,55 @@ args <- commandArgs(T)
 
 input_file <- args[1]
 comparisons <- args[2]
-samples <- args[3]
+samples_file <- args[3]
 output_name <- args[4]
+dataset_label <- args[5]
 
 # read table with DE_gene_counts
 up.down.genes <- read_delim(input_file, delim = " ", col_names = F, trim_ws = T) %>% 
-  mutate(comparison=as.numeric(str_extract(X1, "(\\d)+"))) %>% 
-  mutate(feature=str_remove(X1, "[:alpha:]+_[:alpha:]+_[:alnum:]+_[:alpha:]+_")) %>% 
-  mutate(package=ifelse(str_detect(str_to_lower(X2), "deseq"),"DESeq","edgeR")) %>% 
-  select(comparison, feature, package, Up_genes = X4, Down_genes = X5)
+  rowwise() %>% 
+  mutate(comparison=as.numeric(str_extract(X1, "(\\d)+")),
+         feature=str_remove(X1, "[:alpha:]+_[:alpha:]+_[:alnum:]+_[:alpha:]+_"),
+         package=ifelse(str_detect(str_to_lower(X2), "deseq"),"DESeq","edgeR"),
+         # Condition_1_GM_Numbers=str_extract_all(X2,"G[:alnum:]+_[:alnum:]+", simplify = T)[2],
+         # Condition_2_GM_Numbers=str_extract_all(X2,"G[:alnum:]+_[:alnum:]+", simplify = T)[1],
+         Dataset_label=dataset_label) %>% 
+  select(Dataset_label, comparison, feature, package, Up_genes = X4, Down_genes = X5)
 
-# read comparison df
+# comparison df
 cmp <- read_delim(comparisons, delim = ";", col_names = T, trim_ws = T) %>% 
   select(comparison = Comparison_Number, Condition_1, Condition_2)
 
-# read samples df
-samples <- read_delim(samples, delim=";", col_names = T, trim_ws = T) %>% 
-  select(Group, Condition_Name) %>% 
+# samples df
+samples <- read_delim(samples_file, delim=";", col_names = T, trim_ws = T) %>% 
+  select(Group, Condition_Name, Sample_ID) %>% 
+  mutate(Sample_ID=str_extract(Sample_ID,"([:alnum:]+)$")) %>% 
+  group_by(Group) %>% 
+  mutate(Samples=paste0(Sample_ID, collapse = ",")) %>% 
+  ungroup() %>% 
+  select(-Sample_ID) %>% 
   distinct()
 
-# join comparison and samples
+# combine comparison and samples
 cmp_samples <- left_join(cmp, samples, by=c("Condition_1"="Group")) %>% 
   left_join(., samples, by=c("Condition_2"="Group")) %>% 
-  select(comparison, Condition_1, Condition_2, Condition_Name_1 = Condition_Name.x, Condition_Name_2 = Condition_Name.y)
+  select(comparison, Condition_1, Condition_2, 
+         Condition_Name_1 = Condition_Name.x, 
+         Condition_Name_2 = Condition_Name.y,
+         Condition_1_GM_Numbers = Samples.x,
+         Condition_2_GM_Numbers = Samples.y)
 
 # final table
 left_join(up.down.genes,cmp_samples) %>% 
-  select(comparison,feature,package,Condition_1,Condition_2,Condition_Name_1,Condition_Name_2,Up_genes,Down_genes) %>% 
-  gather(variable, value, -(comparison:Condition_Name_2)) %>% 
+  select(Dataset_label, comparison,feature,package,Condition_1,Condition_2,
+         Condition_Name_1,Condition_Name_2,
+         Condition_1_GM_Numbers,Condition_2_GM_Numbers,
+         Up_genes,Down_genes) %>% 
+  gather(variable, value, -(Dataset_label:Condition_2_GM_Numbers)) %>% 
   unite(combine,feature,variable, sep = ".") %>% 
   spread(combine, value) %>% 
   arrange(comparison) %>% 
-  write_csv(., output_name)
+  write_excel_csv2(., output_name)
+
+
+
