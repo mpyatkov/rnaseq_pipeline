dda99cd5de7b4695531c9ff843616703b3abae11
mpyatkov <test@test.com>
2020-09-30 15:04:00 -0400

    removed 08ab and add one 08

---
+++ Scripts/08_Extract_Counts/Run_Jobs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -l
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+rm -rf ./logs && mkdir -p ./logs
+
+# export all variables from Pipeline_Setup.conf
+eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
+
+# load anaconda module for case when we need independent run
+set +eu
+module load anaconda2
+source activate RNAseq
+set -eu
+
+# dir_name and job_name are required in the next steps
+dir_name=$(basename $(pwd))
+step_num=$(echo ${dir_name} | cut -d'_' -f 1)
+job_name="${DATASET_LABEL}_${step_num}"
+
+# samples contains array of (sample_dir, sample_id, description) for each sample
+samples=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --samples))
+gtflist=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --gtf_annotation_and_counter))
+
+# loop over all samples
+for ((i=0;i< ${#samples[@]} ;i+=3))
+do
+    sample_dir=${samples[i]}
+    sample_id=${samples[i+1]}
+    description=${samples[i+2]}
+    
+    for ((j=0; j< ${#gtflist[@]} ;j+=2))
+    do
+        gtf_file=${gtflist[j]}
+        counter=${gtflist[j+1]}
+        (set -x; qsub -N "${job_name}_${sample_id}" -P "${PROJECT}" -l h_rt="04:00:00" Extract_Counts.qsub ${sample_id} ${gtf_file} ${counter})
+    done
+    
+done
+
+echo "End of 8 step commands"
+echo "-----------------------"
+
---
+++ Scripts/08_Extract_Counts/Summarize_Jobs.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# export all variables from Pipeline_Setup.conf
+eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
+
+# output directory
+summary_dir="$(pwd)/Job_Summary"
+rm -rf "${summary_dir}" && mkdir -p "${summary_dir}"
+
+# gtflist
+gtflist=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --gtf_annotation_and_counter))
+
+# samples contains array of (sample_dir, sample_id, description) for each sample
+samples=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --samples))
+
+for ((i=0;i< ${#gtflist[@]} ;i+=2));
+do
+    gtf_file=${gtflist[i]}
+    counter=${gtflist[i+1]}
+
+    eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export_gtf_by_name_and_counter ${gtf_file} ${counter})"
+
+    OUTPUT_FILE=${summary_dir}/${COUNTER}_Stats_${OUTPUT_DIR}.txt
+    rm -rf ${OUTPUT_FILE} && touch ${OUTPUT_FILE}
+    
+    if [[ $COUNTER == "featureCounts" ]]
+    then
+	OUTPUT_FILE_SPECIAL="${summary_dir}/${COUNTER}_summary_${OUTPUT_DIR}.txt"
+	rm -rf ${OUTPUT_FILE} && touch ${OUTPUT_FILE}
+    fi
+
+    ## REM_SPLICE used only because we need only intronic gtf files
+    if [[ $REM_SPLICE_JUNC != 0 ]]
+    then
+        echo 'SAMPLE_ID' $'\t''DESCRIPTION' $'\t''Aligned_Pairs' $'\t''Read_Pair_IN_INTRONS' $'\t''Read_Pair_IN_INTRONS_RATIO' >> $OUTPUT_FILE
+    else
+        echo 'SAMPLE_ID' $'\t''DESCRIPTION' $'\t''Aligned_Pairs' $'\t''Read_Pair_IN_EXONS' $'\t''Read_Pair_IN_EXONS_RATIO' >> $OUTPUT_FILE
+    fi
+    
+    for ((j=0;j<${#samples[@]} ;j+=3));
+    do
+        sample_dir=${samples[j]}
+        sample_id=${samples[j+1]}
+        description=${samples[j+2]}
+
+        pushd "${DATASET_DIR}/${sample_id}/tophat2"
+
+        Aligned_Pairs=$(grep "Aligned pairs:"  "${sample_id}_align_summary.txt"  | awk '{print $3}')
+        counter_DIR="${DATASET_DIR}/${sample_id}/tophat2/${COUNTER}"
+            
+        pushd "${counter_DIR}/${OUTPUT_DIR}"
+	
+
+	# READS_IN_EXONS=$(awk '{n+=$2;} ; END {print n;}' "${sample_id}_${COUNTER}.out")
+	if [[ $COUNTER == "htseq" ]]
+	then
+	    # need to remove last 5 lines before processing
+	    READS_IN_EXONS=$(cat "${sample_id}_${COUNTER}.out" | head -n-5 | awk '{n+=$2;} ; END {print n;}')
+        else
+	    READS_IN_EXONS=$(awk '{n+=$2;} ; END {print n;}' "${sample_id}_${COUNTER}.out")
+	fi
+
+        # Calculate percentages
+        READS_IN_EXONS_RATIO=$(echo "scale=4;$READS_IN_EXONS/$Aligned_Pairs" | bc)
+        echo $sample_id $'\t'$description $'\t'$Aligned_Pairs $'\t'$READS_IN_EXONS $'\t'$READS_IN_EXONS_RATIO >> $OUTPUT_FILE
+	popd
+	popd
+    done
+
+    if [[ $COUNTER == "htseq" ]]
+    then
+	echo 'SAMPLE_ID' $'\t''DESCRIPTION' $'\t''Aligned_Pairs' $'\t''no_feature_count' $'\t''no_feature_RATIO' $'\t''ambiguous_count'$'\t''ambiguous_RATIO' $'\t''too_low_aQual_count'$'\t''too_low_aQual_RATIO' $'\t''not_aligned_count'$'\t''not_aligned_RATIO' $'\t''alignment_not_unique'$'\t''alignment_not_unique_RATIO' >> ${OUTPUT_FILE}
+    fi
+
+    for ((j=0; j<${#samples[@]} ;j+=3));
+    do
+        sample_dir=${samples[j]}
+        sample_id=${samples[j+1]}
+        description=${samples[j+2]}
+
+        pushd "${DATASET_DIR}/${sample_id}/tophat2"
+
+        Aligned_Pairs=$(grep "Aligned pairs:"  "${sample_id}_align_summary.txt"  | awk '{print $3}')
+        counter_DIR="${DATASET_DIR}/${sample_id}/tophat2/${COUNTER}"
+	pushd "${counter_DIR}/${OUTPUT_DIR}"
+
+	if [[ $COUNTER == "htseq" ]]
+	then
+
+	    echo 'Getting no_feature count...'
+            no_feature=$(grep '__no_feature' "${sample_id}_${COUNTER}.out"  | awk '{print $2}')
+            
+            echo 'Calculate percentage...'
+            no_feature_RATIO=$(echo "scale=4;$no_feature/$Aligned_Pairs" | bc)
+            
+            echo 'Getting ambiguous count...'
+            ambiguous=$(grep '__ambiguous' "${sample_id}_${COUNTER}.out"  | awk '{print $2}')
+            
+            echo 'Calculate percentage...'
+            ambiguous_RATIO=$(echo "scale=4;$ambiguous/$Aligned_Pairs" | bc)
+            
+            echo 'Getting too_low_aQual count...'
+            too_low_aQual=$(grep '__too_low_aQual' "${sample_id}_${COUNTER}.out"  | awk '{print $2}')
+            echo 'Calculate percentage...'
+            too_low_aQual_RATIO=$(echo "scale=4;$too_low_aQual/$Aligned_Pairs" | bc)
+            
+            echo 'Getting not_aligned count...'
+            not_aligned=$(grep '__not_aligned' "${sample_id}_${COUNTER}.out"  | awk '{print $2}')
+            echo 'Calculate percentage...'
+            not_aligned_RATIO=$(echo "scale=4;$not_aligned/$Aligned_Pairs" | bc)
+            
+            echo 'Getting alignment_not_unique count...'
+            alignment_not_unique=$(grep '__alignment_not_unique' "${sample_id}_${COUNTER}.out"  | awk '{print $2}')
+            
+            echo 'Calculate percentage...'
+            alignment_not_unique_RATIO=$(echo "scale=4;$alignment_not_unique/$Aligned_Pairs" | bc)
+            echo 'Printing to output file...'
+            echo $sample_id $'\t'$description $'\t'$Aligned_Pairs $'\t'$no_feature $'\t'$no_feature_RATIO $'\t'$ambiguous $'\t'$ambiguous_RATIO $'\t'$too_low_aQual $'\t'$too_low_aQual_RATIO $'\t'$not_aligned $'\t'$not_aligned_RATIO $'\t'$alignment_not_unique $'\t'$alignment_not_unique_RATIO >> ${OUTPUT_FILE}
+
+	else
+	    
+	    awk 'NR>1' "${sample_id}_${COUNTER}.out.summary" > temp1.txt
+	                
+            #Divide 2nd column by total number of reads:
+            awk -v Aligned_Pairs=${Aligned_Pairs} '{ Ratio=($2)/(Aligned_Pairs) ; print $1"\t"$2"\t"Aligned_Pairs"\t"Ratio }' temp1.txt > temp2.txt
+            #Need a header:
+            echo 'Status' $'\t'$sample_id'_Read_Count' $'\t''Aligned_Pairs' $'\t''Ratio' >> Header.txt
+            #Combine files:
+            cat Header.txt temp2.txt > temp3.txt
+
+            # if [ "${ANNOTATION_FILE}" == "RefSeq_GeneBody.gtf" ];
+            # then
+            #     sed 's/.out.*/:/' temp3.txt > temp4.txt
+            #     mv temp4.txt temp3.txt
+            # fi
+
+            echo 'Concatenating summary file...'
+            cat temp3.txt >> $OUTPUT_FILE_SPECIAL
+
+            #Remove temp files:
+            rm temp*.txt
+            rm Header.txt
+
+	fi
+	popd
+	popd
+    done
+done
