ce6aebcc67500cff09acdb8ef1639444327970b3
mpyatkov <test@test.com>
2020-09-30 16:07:28 -0400

    refactored step 14

--- Scripts/14_final_summary/Run_Jobs.sh
+++ Scripts/14_final_summary/Run_Jobs.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/bash -l
 ######################################################################################
 # Kritika Karri, 08.04.2017
 # Way to run script:
@@ -12,8 +12,6 @@ set -o pipefail
 set -o nounset
 
 module load gcc/8.1.0
-#module load R/3.1.1
-#module load R/3.2
 module load R/3.6.0
 
 # export all variables from Pipeline_Setup.conf
@@ -26,13 +24,6 @@ rm -rf DiffExp_*
 rm -rf output 
 
 SCRIPT_DIR="$(pwd)"
-
-echo "DATASET_DIR:"
-echo ${DATASET_DIR}
-echo "DATASET_LABEL:"
-echo ${DATASET_LABEL}
-echo "SCRIPT_DIR:"
-echo ${SCRIPT_DIR}
 cd ..
 
 Level_UP=$(pwd)
@@ -45,92 +36,75 @@ echo ${SCRIPT_DIR}
 echo "Level_UP:"
 echo ${Level_UP}
 
-cp -rf  ${Level_UP}/04_TopHat_Paired_End/Job_Summary/TopHat2_Stats_BestMapped.txt  .
-cp -rf  ${Level_UP}/05_Read_Strandness/Job_Summary/Read_Strandness_Stats.txt .
-cp -rf  ${Level_UP}/06_CollectRnaSeqMetrics/Job_Summary/CollectRnaSeqMetrics_Stats.txt  .
-cp -rf  ${Level_UP}/07_CollectInsertSizeMetrics/Job_Summary/CollectInsertSizeMetrics_Plots.pdf .
-cp -rf  ${Level_UP}/08b_Extract_Counts_featureCounts/Job_Summary/featureCounts_summary_Illumina_GTF.txt  .
-cp -rf  ${Level_UP}/08b_Extract_Counts_featureCounts/Job_Summary/featureCounts_summary_LncRNA_Exon_Collapsed_GTF.txt  .
-# cp -rf ${Level_UP}/13b_Pearson_Correlation_featureCounts/PCA* .
-# cp -rf ${Level_UP}/13c_Pearson_Correlation_lncRNA/PCA* .
+cp -rf  ${Level_UP}/04_TopHat_Paired_End/Job_Summary/TopHat2_Stats_BestMapped.txt  ./output/
+cp -rf  ${Level_UP}/05_Read_Strandness/Job_Summary/Read_Strandness_Stats.txt ./output/
+cp -rf  ${Level_UP}/06_CollectRnaSeqMetrics/Job_Summary/CollectRnaSeqMetrics_Stats.txt  ./output/
+cp -rf  ${Level_UP}/07_CollectInsertSizeMetrics/Job_Summary/CollectInsertSizeMetrics_Plots.pdf ./output/
+cp -rf  ${Level_UP}/08_Extract_Counts/Job_Summary/featureCounts_summary_LncRNA15k_ExonCollapsed_GTF.txt  ./output/
 cp -rf ${Level_UP}/13_Correlation/Job_Summary/* ./output/
 find ./output/13* -name "*.pdf" | grep -iv "combined" | xargs rm -rf
 find ./output/13* \( -name "*.csv" -o -name "*.txt" \) | xargs rm -rf
 
-# mv *.txt ${Level_UP}/14_final_summary/output
-mv *.pdf ${Level_UP}/14_final_summary/output
-
 # remove from 09abc/Output* all segex files
+# TODO: refactor this, data should be removed in the DE step
 set +eu
 find ../09* -name "*_forSEGEXUpload*.txt" | grep Output | xargs -n1 rm -rf 
 set -eu
 
+copy_feature(){
+    local de_index=$1
+    local feature=$2
+    mkdir -p output/Segex_${de_index}/Segex${de_index}_${feature}
+    find ../${de_index}_DE_* -name "*SEGEX*" | grep -iv output | grep -i ${feature} | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_${de_index}/Segex${de_index}_${feature}/
+}
+
 # copy all segex files in 
 # 09a 
 mkdir -p output/Segex_09a/
-find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09a/
+find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i exoncollapsed | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09a/
 
-mkdir -p output/Segex_09a/Segex09a_GeneBody=ExonCollapsed
-find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i edger | grep -i genebody | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_GeneBody=ExonCollapsed/
+copy_feature 09a ExonCollapsed
+copy_feature 09a IntronOnly
+copy_feature 09a ExonOnly
 
-mkdir -p output/Segex_09a/Segex09a_Intronic_Only
-find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i edger | grep -i intronic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Intronic_Only/
-
-mkdir -p output/Segex_09a/Segex09a_Exonic_Only
-find ../09a_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i edger | grep -i exonic | xargs -n1 -I{} cp {} ./output/Segex_09a/Segex09a_Exonic_Only/
-
-# 09b
-
+# # 09b
 mkdir output/Segex_09b
-find ../09b_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i genebody | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/
+find ../09b_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i exoncollapsed | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/
 
-mkdir -p output/Segex_09b/Segex09b_GeneBody=ExonCollapsed
-find ../09b_DE_* -name "*SEGEX*" | grep -i genebody | grep -iv output | grep -i fpkm | grep -i edger |  xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_GeneBody=ExonCollapsed/
+copy_feature 09b ExonCollapsed
+copy_feature 09b IntronOnly
+copy_feature 09b ExonOnly
 
-mkdir -p output/Segex_09b/Segex09b_Intronic_Only
-find ../09b_DE_* -name "*SEGEX*" | grep -i intronic | grep -iv output | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Intronic_Only/
 
-mkdir -p output/Segex_09b/Segex09b_Exonic_Only
-find ../09b_DE_* -name "*SEGEX*" | grep -i exonic | grep -iv output | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09b/Segex09b_Exonic_Only/
-
-# 09c
-
+# # 09c
 mkdir output/Segex_09c
 find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i fpkm | grep -i exoncoll | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/
 
-mkdir -p output/Segex_09c/Segex09c_ExonCollapsed
-find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i exoncoll | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_ExonCollapsed/
-
-mkdir -p output/Segex_09c/Segex09c_GeneBody
-find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i genebody | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_GeneBody/
-
-mkdir -p output/Segex_09c/Segex09c_Intronic_Only
-find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i intronic | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Intronic_Only/
-
-mkdir -p output/Segex_09c/Segex09c_Exonic_Only
-find ../09c_DE_* -name "*SEGEX*" | grep -iv output | grep -i exonic | grep -i fpkm | grep -i edger | xargs -n1 -I{} cp {} ./output/Segex_09c/Segex09c_Exonic_Only/
+copy_feature 09c ExonCollapsed
+copy_feature 09c IntronicOnly
+copy_feature 09c ExonicOnly
+copy_feature 09c FullGeneBody
 
 # summarize up and down genes
-# 09a
+function updown_genes() {
+    local de_index=$1
+    
+    INPUT_UPDOWN_FILE="./gene_counts.csv"
+    SAMPLES_FILE="../00_Setup_Pipeline/Sample_Labels.txt"
+    CMP_FILE="../00_Setup_Pipeline/Comparisons.txt"
+    
+    OUTPUT_FILE="$(pwd)/${de_index}_DE_Genes_counts.csv"
+    find ../"${de_index}"_DE_* -iname  "*DE_Gene_C*" | xargs -n1 -I{} cat {} | grep -v "Output_File" >> ${INPUT_UPDOWN_FILE}
+    Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE} ${DATASET_LABEL}
+    mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
 
-INPUT_UPDOWN_FILE="./gene_counts.csv"
-SAMPLES_FILE="../00_Setup_Pipeline/Sample_Labels.txt"
-CMP_FILE="../00_Setup_Pipeline/Comparisons.txt"
+}
 
-OUTPUT_FILE="$(pwd)/09a_DE_Genes_counts.csv"
-find ../09a_DE_* -iname  "*DE_Gene_C*" | xargs -n1 -I{} cat {} | grep -v "Output_File" >> ${INPUT_UPDOWN_FILE}
-Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE} ${DATASET_LABEL}
-mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
+updown_genes 09a
+updown_genes 09b
+updown_genes 09c
 
-OUTPUT_FILE="$(pwd)/09b_DE_Genes_counts.csv"
-find ../09b_DE_* -iname "*DE_Gene_C*" | xargs -n1 -I{} cat {} | grep -v "Output_File" >> ${INPUT_UPDOWN_FILE}
-Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE} ${DATASET_LABEL}
-mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
+# mv *.txt ${Level_UP}/14_final_summary/output
+# mv *.pdf ${Level_UP}/14_final_summary/output
 
-OUTPUT_FILE="$(pwd)/09c_DE_Genes_counts.csv"
-find ../09c_DE_* -iname "*DE_Gene_C*" | xargs -n1 -I{} cat {} | grep -v "Output_File" >> ${INPUT_UPDOWN_FILE}
-Rscript ./Scripts/updown_genes.R ${INPUT_UPDOWN_FILE} ${CMP_FILE} ${SAMPLES_FILE} ${OUTPUT_FILE} ${DATASET_LABEL}
-mv ${OUTPUT_FILE} ./output && rm ${INPUT_UPDOWN_FILE}
-
-mv *.txt ${Level_UP}/14_final_summary/output	
 echo "All files copied. Done "
