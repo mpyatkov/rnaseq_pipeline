2dcf5f48d83886497eb25ab962cecb03be454d53
mpyatkov <test@test.com>
2020-10-09 10:33:44 -0400

    fix: return usual name Step for job_name

--- Scripts/08_Extract_Counts/Run_Jobs.sh
+++ Scripts/08_Extract_Counts/Run_Jobs.sh
@@ -12,7 +12,7 @@ eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
 # dir_name and job_name are required in the next steps
 dir_name=$(basename $(pwd))
 step_num=$(echo ${dir_name} | cut -d'_' -f 1)
-job_name="${DATASET_LABEL}_${step_num}"
+job_name="Step_${step_num}"
 
 # samples contains array of (sample_dir, sample_id, description) for each sample
 samples=($("${SETUP_PIPELINE_DIR}"/01_Pipeline_Setup.py --samples))
@@ -29,7 +29,7 @@ do
     do
         gtf_file=${gtflist[j]}
         counter=${gtflist[j+1]}
-        (set -x; qsub -N "${job_name}_${sample_id}" -P "${PROJECT}" -l h_rt="04:00:00" Extract_Counts.qsub ${sample_id} ${gtf_file} ${counter})
+        (set -x; qsub -N "${job_name}_${sample_id}" -P "${PROJECT}" -l h_rt="24:00:00" Extract_Counts.qsub ${sample_id} ${gtf_file} ${counter})
     done
     
 done
