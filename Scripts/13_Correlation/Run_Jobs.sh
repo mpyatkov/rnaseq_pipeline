bd87a76795f40c0ec24d11b9d4cbb822337b27d2
mpyatkov <test@test.com>
2020-11-29 17:04:50 -0500

    fix: added parallel version of Step 13 which utilize 09* folders contained ExonCollapsed files

--- Scripts/13_Correlation/Run_Jobs.sh
+++ Scripts/13_Correlation/Run_Jobs.sh
@@ -10,7 +10,19 @@ rm -rf ./logs && mkdir -p ./logs
 # export all variables from Pipeline_Setup.conf
 eval "$(../00_Setup_Pipeline/01_Pipeline_Setup.py --export)"
 
-(set -x; qsub -N "Step_13" -P "${PROJECT}" -l h_rt="04:00:00" correlation.qsub)
+rm -rf Job_Summary && mkdir -p Job_Summary
+
+#dedirs=$(find ../ -maxdepth 1 -name "09*" -type d | xargs -n1  basename |cut -d "_" -f1 | sort | uniq)
+
+# extract all 09* dirs which contain ExonCollapsed subfolders
+dedirs=$(find ../09* -iname "output*exoncoll*" | grep -Po '09[a-z]' | sort | uniq)
+
+for d in $dedirs
+do
+    (set -x; qsub -N "Step_13_$d" -P "${PROJECT}" -l h_rt="04:00:00" correlation.qsub $d)
+done
+
+# (set -x; qsub -N "Step_13" -P "${PROJECT}" -l h_rt="04:00:00" correlation.qsub)
 
 echo "End of 13_Correlation"
 echo "-----------------------"
