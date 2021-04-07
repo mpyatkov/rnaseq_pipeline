4df8a5db9c6a434ea6a4733423e57767b6f32a48
mpyatkov <test@test.com>
2021-04-06 20:54:04 -0400

    Removed outdated stuff from 10/scripts/TSV...R

--- Scripts/10_TACO/Scripts/TSV_processing.R
+++ Scripts/10_TACO/Scripts/TSV_processing.R
@@ -21,18 +21,3 @@ read_tsv(paste0(INPUT_PREFIX,".tsv"), col_names = T) %>%
   separate(transcript_id, into = c("strand_tmp", "gene_id", "tr_id","expr","abs_frac"), sep = "_", convert = T) %>% 
   select(gene_id, transcript_id = tr_id, expr, abs_frac, everything(), -strand_tmp) %>% 
   write_tsv(paste0(OUTPUT_PREFIX,".tsv"))
-
-# 
-# orig <- read_tsv(paste0(INPUT_PREFIX,".tsv"), col_names = T)
-# 
-# new <- orig %>% 
-#   select(transcript_id) %>% 
-#   mutate(transcript_id = prepare_for_separation(transcript_id)) %>% 
-#   separate(transcript_id, into = c("strand", "gene_id", "tr_id","expr","abs_frac"), sep = "_", convert = T) %>% 
-#   write_tsv()
-
-# mutate(strand = str_extract(transcript_id,"\\+|\\-"),
-#        gene_id = str_extract(transcript_id,"(?<=\\))([[:alnum:]]+)(?=\\_)"),
-#        transcript_id = str_extract(transcript_id,"(?<=\\_)([[:alnum:]]+)(?=\\()"),
-#        expr = as.double(str_extract(transcript_id,"(?<=\\()([[:digit:]|\\.]+)(?=\\))")),
-#        abs_frac = as.double(str_extract(transcript_id,"(?<=\\()([[:digit:]|\\.]+)(?=\\)$)"))) %>% 
